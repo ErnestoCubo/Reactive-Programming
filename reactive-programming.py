5adebbfc2b3a6245b78df945705fe788ecaaa950
Estiwer <30570774+ErnestoNoihara@users.noreply.github.com>
2021-12-15 02:27:43 +0100

    Funciona con certificados SSL

--- reactive-programming.py
+++ reactive-programming.py
@@ -4,12 +4,23 @@ import asyncio
 import aiofiles
 import io
 import rx
+import sys
 from tkinter import *
 from tkinter.ttk import *
 from urllib.request import urlopen
 from PIL import ImageTk, Image
 from bs4 import BeautifulSoup
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 class App:
 
@@ -24,8 +35,17 @@ class App:
         imgSrc = self.bytesfoto[index]
         self.img = ImageTk.PhotoImage(data = imgSrc)
         self.canvas.create_image(20,20, anchor=NW, image=self.img)  
+
     
     async def main(self, urlToProcess):
+        
+        def checkImgAlt(imgName, imgSrc):
+            urlSplit = imgSrc.split('/')
+            name = urlSplit.pop()
+            if len(imgName) <= 0 :
+                imgName = name
+            return imgName
+            
         async def getSourceCode(url):
             async with aiohttp.ClientSession() as clientSession:
                 serverResponse = await clientSession.get(url)
@@ -34,24 +54,32 @@ class App:
                 
         sourceCode = await getSourceCode(urlToProcess)
         parsedSource = BeautifulSoup(sourceCode, 'html.parser')
+        
         for imgItem in parsedSource.find_all('img', src=True):
-            imgName = imgItem['alt']
+            
+            try:
+                imgName = imgItem['alt']
+            except:
+                imgName = ''
             imgSrc = imgItem['src']
-            if imgSrc.find('http') == True:
-                if len(imgName) < 1 :
-                    imgName = imgSrc
-            elif imgSrc.startswith('//'):
-                imgSrc = 'https:' + imgSrc
-            else:
-                urlSplit = urlToProcess.split('/')
-                rootUrlPath = urlSplit[0] + '//' + urlSplit[2]
-                imgSrc = rootUrlPath + imgSrc
-                if len(imgItem['alt']) > 0 :
-                    imgName = imgItem['alt']
+                  
+            if imgSrc.find('http') == -1:
+                if imgSrc.startswith('//'):
+                    
+                    imgSrc = 'https:' + imgSrc
                 else:
-                    imgName =  imgSrc
+                    urlSplit = urlToProcess.split('/')
+                    rootUrlPath = urlSplit[0] + '//' + urlSplit[2]
+                    imgSrc = rootUrlPath + imgSrc
+            
+            imgName = checkImgAlt(imgName, imgSrc)
+            
             if not imgName in self.imgs:
-                async with aiohttp.ClientSession() as clientSession:
+                
+                #Como apunte el connector se trata de un checker para el scertificado en este caso como muchas páginas no verifican este 
+                # certificado pues podemos también no verificarlo y de esta manera poder pasar el filtro y con trust_env=True 
+                # poder decirle al prgrama que en este caso queremos que confí en la web siempre    
+                async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False) ,trust_env=True) as clientSession:
                     serverResponse = await clientSession.get(imgSrc)
                     responseStatus = serverResponse.status
                     imgBytes = await serverResponse.read()
@@ -60,9 +88,13 @@ class App:
                     # porque lo es implicitamente en la petición anterior al servidor
                     if responseStatus == 200:
                         self.bytesfoto.append(imgBytes)
+                        sys.stdout.write(bcolors.OKGREEN)
                         print(f'{imgName} : {imgSrc} -> STATUS OK : {responseStatus}')
+                        sys.stdout.write(bcolors.ENDC)
                     else:
+                        sys.stdout.write(bcolors.FAIL)
                         print(f'{imgName} : {imgSrc} -> STATUS FAILED : {responseStatus}')
+                        sys.stdout.write(bcolors.ENDC)
 
     """
     Definicion de los atributos de la clase iniciales donde se guardan los datos que se usaran en el tkinter para la GUI
